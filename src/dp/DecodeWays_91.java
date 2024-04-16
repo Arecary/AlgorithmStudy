3c77ee35ef2d03de3d0eab1a090537e08f404ac8
Areca <102679138+Arecary@users.noreply.github.com>
2024-04-15 23:16:35 -0400

    91

---
+++ src/dp/DecodeWays_91.java
@@ -0,0 +1,15 @@
+package dp;
+
+public class DecodeWays_91 {
+
+
+  // 规定1和A对应，2和B对应，3和C对应...，那么一个数字字符串比如"111"就可以转化为"AAA","KA","AK"。
+  // 那么给定一个只有数字组成的字符串str，返回有多少种转化结果。
+
+
+  // 递归版本
+  public int numDecodings(String s) {
+
+  }
+
+}
