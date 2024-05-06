9a203ec13b6ef55af30a258a269c03292953e9a0
Areca <102679138+Arecary@users.noreply.github.com>
2024-05-06 19:54:02 -0400

    42, 84

--- src/monotonicstack/LargestRectangleHistogram_84.java
+++ src/monotonicstack/LargestRectangleHistogram_84.java
@@ -1,2 +1,50 @@
-package monotonicstack;public class LargestRectangleHistogram_84 {
+package monotonicstack;
+
+import java.util.Stack;
+
+public class LargestRectangleHistogram_84 {
+
+  // 这题是找左右小的值，所以是单调递减栈
+
+  int largestRectangleArea(int[] heights) {
+    Stack<Integer> st = new Stack<Integer>();
+
+    // 数组扩容，在头和尾各加入一个元素，为了防止出现数组本来就是单调递增，
+    // 那么加入stack时就会直接全部加进去而不走到计算面积的那一步，所以补一个0在末尾，这样一定会触发计算面积的code。
+    // 或者数组开头2个元素是递减，直接触发计算面积的code，但是计算面积需要三个元素，所以补一个0在开头。
+    int [] newHeights = new int[heights.length + 2];
+    newHeights[0] = 0;
+    newHeights[newHeights.length - 1] = 0;
+    for (int index = 0; index < heights.length; index++){
+      newHeights[index + 1] = heights[index];
+    }
+
+    heights = newHeights;
+
+    st.push(0);
+    int result = 0;
+    // 第一个元素已经入栈，从下标1开始
+    for (int i = 1; i < heights.length; i++) {
+      // 注意heights[i] 是和heights[st.top()] 比较 ，st.top()是下标
+      if (heights[i] > heights[st.peek()]) {
+        st.push(i);
+      } else if (heights[i] == heights[st.peek()]) {
+        st.pop(); // 这个可以加，可以不加，效果一样，思路不同
+        st.push(i);
+      } else {
+        while (heights[i] < heights[st.peek()]) { // 注意是while
+          int mid = st.peek();
+          st.pop();
+          int left = st.peek();
+          int right = i;
+          int w = right - left - 1;
+          int h = heights[mid];
+          result = Math.max(result, w * h);
+        }
+        st.push(i);
+      }
+    }
+    return result;
+  }
+
 }
--- src/monotonicstack/TrappingRainWater_42.java
+++ src/monotonicstack/TrappingRainWater_42.java
@@ -1,2 +1,54 @@
-package monotonicstack;public class TrappingRainWater_42 {
+package monotonicstack;
+
+import java.util.Stack;
+
+public class TrappingRainWater_42 {
+
+  // 思路：
+  // 找右边第一个大的数和左边第一个大，这样就能算当前元素的雨水
+  // 左边第一个大的数就是单调栈的栈顶的后面一个数
+  // 当出现current元素比栈顶大的时候，栈顶元素就是中间那个最小的，current就是右边第一个大的，而栈顶元素后面一个就是左边第一个大的，因为是单调递增栈
+
+
+  public int trap(int[] height){
+    int size = height.length;
+
+    if (size <= 2) return 0;
+
+    // in the stack, we push the index of array
+    // using height[] to access the real height
+    Stack<Integer> stack = new Stack<Integer>();
+    stack.push(0);
+
+    int sum = 0;
+    for (int index = 1; index < size; index++){
+      int stackTop = stack.peek();
+      if (height[index] < height[stackTop]){
+        stack.push(index);
+      }else if (height[index] == height[stackTop]){
+        // 因为相等的相邻墙，左边一个是不可能存放雨水的，所以pop左边的index, push当前的index
+        stack.pop();
+        stack.push(index);
+      }else{
+        //pop up all lower value
+        int heightAtIdx = height[index];
+        while (!stack.isEmpty() && (heightAtIdx > height[stackTop])){
+          int mid = stack.pop();
+
+          if (!stack.isEmpty()){
+            int left = stack.peek();
+
+            int h = Math.min(height[left], height[index]) - height[mid];
+            int w = index - left - 1;
+            int hold = h * w;
+            if (hold > 0) sum += hold;
+            stackTop = stack.peek();
+          }
+        }
+        stack.push(index);
+      }
+    }
+
+    return sum;
+  }
 }
