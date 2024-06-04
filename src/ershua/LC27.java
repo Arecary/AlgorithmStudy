6e11fc7c94cbe5410b755007b68d55a2c117233a
Areca <102679138+Arecary@users.noreply.github.com>
2024-06-04 18:07:21 -0400

    704 27

--- src/ershua/LC27.java
+++ src/ershua/LC27.java
@@ -2,4 +2,36 @@ package ershua;
 
 public class LC27 {
 
+    public int removeElement(int[] nums, int val) {
+        int last = nums.length - 1;
+        int length = nums.length;
+        for(int i= 0; i <= last;){
+            if (nums[i] == val) {
+                length--;
+                nums[i] = nums[last];
+                last--;
+            }else {
+                i++;
+            }
+        }
+        
+        return length;
+    }
+
+
+    public int removeElement2(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            if(nums[left] == val){
+                nums[left] = nums[right];
+                right--;
+            }else {
+                // 这里兼容了right指针指向的值与val相等的情况
+                left++;
+            }
+        }
+        return left;
+    }
+
 }
--- src/ershua/LC704.java
+++ src/ershua/LC704.java
@@ -4,7 +4,27 @@ public class LC704 {
 
 
   public int search(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    
+
+    return bs(nums, target, left, right);
+
+  }
 
 
+  public int bs(int[] nums, int target, int left, int right){
+
+    if (left > right) {
+      return -1;
+    }
+    int mid = left + (right - left) / 2;
+    if (nums[mid] == target) {
+      return mid;
+    }else if (nums[mid] > target) {
+      return bs(nums, target, left, mid-1);
+    }else {
+      return bs(nums, target, mid+1, right);
+    }
   }
 }
