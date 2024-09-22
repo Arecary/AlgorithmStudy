2ddb61c84ba135c20f5eec04ebbbea2125794953
Areca <102679138+Arecary@users.noreply.github.com>
2024-09-22 18:53:47 -0400

    glint work

--- src/glint/homework/Glint0909.java
+++ src/glint/homework/Glint0909.java
@@ -2,6 +2,30 @@ package glint.homework;
 
 public class Glint0909 {
 
+  public static void main(String[] args) {
+
+    int n = 7;
+    printStarPattern(n);
+
+  }
+
+  public static void printStarPattern(int n) {
+    int mid = (n + 1) / 2;
+
+    for (int i = 1; i <= mid; i++) {
+      for (int j = 1; j <= 2 * i - 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+    for (int i = mid - 1; i >= 1; i--) {
+      for (int j = 1; j <= 2 * i - 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
 
   public boolean isAgeValidToDrive(int age) {
     boolean isValid = true;
